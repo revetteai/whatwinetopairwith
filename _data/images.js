cabe149605b0d2ddaa50952c0f2879864c78bad2
revetteai <revetteai@gmail.com>
2025-05-28 13:21:16 -0500

    Create images.js

---
+++ _data/images.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = () => {
+  // Adjust path to where your images are stored, relative to this file!
+  const imgDir = path.join(__dirname, "../images");
+  return fs.readdirSync(imgDir).filter(file =>
+    /\.(jpg|jpeg|png|webp)$/i.test(file)
+  );
+};
