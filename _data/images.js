dff544bea6d917775cc535b8998d8e15c1521662
revetteai <revetteai@gmail.com>
2025-05-28 13:30:07 -0500

    Update images.js

--- _data/images.js
+++ _data/images.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = () => {
   // Adjust path to where your images are stored, relative to this file!
-  const imgDir = path.join(__dirname, "../images");
+  const imgDir = path.join(__dirname, "../assests/images");
   return fs.readdirSync(imgDir).filter(file =>
     /\.(jpg|jpeg|png|webp)$/i.test(file)
   );
